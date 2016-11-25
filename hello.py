a177e11acfc1aa25ebfb6d7384295da24af0d250
eigenvector7 <nish_c@live.com>
2016-11-25 07:31:34 -0500

    comparing relative speed of list operations vs dequeue operations

--- hello.py
+++ hello.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
 from datetime import datetime
+from collections import deque
+import time
+import random
+
 
 print("Hello World - trying out git from within PyCharm CE")
 
 hour = datetime.now().hour
+
+minuteQ = deque()
+minuteL = list()
+
+# Compare time difference between deque vs list operations
+#TODO - profile using CPU time
+numIterations = 1000000
+
+starttime = time.time()
+for i in range(numIterations):
+#    idx = random.randrange(0,numIterations)
+    minuteL.append(i)
+lTimeDelta = time.time()-starttime
+print("List time {} ".format(lTimeDelta))
+
+starttime = time.time()
+for i in range(numIterations):
+#    idx = random.randrange(0,numIterations)
+    minuteQ.appendleft(i)
+qTimeDelta = time.time()-starttime
+
+print("Q time {} ".format(qTimeDelta))
+
+
+
 
 if hour < 12:
     print("Good Morning, it's {0} o'clock".format(hour))
@@ -11,6 +40,8 @@ elif hour == 12:
     print("Good Noon, it's exactly {0} o'clock".format(hour))
 else:
     print("Good Evening it's {} o'clock".format(hour))
+
+
 
 
 
