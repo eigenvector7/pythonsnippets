ade786c278ba42ef55a25cb816956ee7453f5e51
eigenvector7 <nish_c@live.com>
2016-11-24 20:15:54 -0500

    This time really Adding the License file + some minor snippet to test out datetime modules

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
-print("Hello WOrld")
+from datetime import datetime
+
+print("Hello World - trying out git from within PyCharm CE")
+
+hour = datetime.now().hour
+
+if hour < 12:
+    print("Good Morning, it's {0} o'clock".format(hour))
+elif hour == 12:
+    print("Good Noon, it's exactly {0} o'clock".format(hour))
+else:
+    print("Good Evening it's {} o'clock".format(hour))
+
+
+
